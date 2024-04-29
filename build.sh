5c3951e1bf9fbaf0337d94d761326b9e733bc827
Marco Zanzottera <marcozanzottera@e.email>
2024-04-30 00:52:25 +0200

    add kdenetwork-filesharing

--- build.sh
+++ build.sh
@@ -17,8 +17,6 @@ RELEASE="$(rpm -E %fedora)"
 rpm-ostree install /tmp/rpms/sunshine-fedora-40-amd64.rpm
 systemctl enable sunshine-workaround.service
 
-
-# Install gnome-boxes
-rpm-ostree install gnome-boxes
-
+# Install kdenetwork-filesharing to enable network share in dolphin
+rpm-ostree install kdenetwork-filesharing
 
