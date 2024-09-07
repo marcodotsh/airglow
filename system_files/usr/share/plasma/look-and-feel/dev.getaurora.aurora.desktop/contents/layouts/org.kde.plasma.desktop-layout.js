c046f771661d16a35d6e0253b1292b7391c88f0e
Marco Zanzottera <marcozanzottera@e.email>
2024-09-08 01:03:24 +0200

    Add panels by default

---
+++ system_files/usr/share/plasma/look-and-feel/dev.getaurora.aurora.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,7 @@
+loadTemplate("org.kde.plasma.desktop.defaultDock")
+loadTemplate("org.kde.plasma.desktop.defaultPanel")
+
+var desktopsArray = desktopsForActivity(currentActivity());
+for( var j = 0; j < desktopsArray.length; j++) {
+    desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+}
