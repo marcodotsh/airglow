7b5dac761c7a4c117202cb6a713a69947968cc29
Marco Zanzottera <marcozanzottera@e.email>
2024-09-07 22:28:55 +0200

    Change plasma widgets default settings

--- build_files/airglow-changes.sh
+++ build_files/airglow-changes.sh
@@ -11,3 +11,21 @@ sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]
 # Set icon theme
 sed -i '/\[Icons\]/,// s/^Theme=.*/Theme=Papirus-Dark/' /usr/share/kde-settings/kde-profile/default/share/config/kdeglobals
 sed -i '/\Icon Theme\]/,// s/^Inherits=.*/Inherits=breeze_cursors,Papirus-Dark/' /usr/share/icons/default/index.theme
+
+# Change pager default settings
+sed -i '/<entry name="displayedText" type="Enum">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>0<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.pager/contents/config/main.xml
+sed -i '/<entry name="showWindowIcons" type="Bool">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>true<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.pager/contents/config/main.xml
+sed -i '/<entry name="showOnlyCurrentScreen" type="Bool">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>true<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.pager/contents/config/main.xml
+sed -i '/<entry name="wrapPage" type="Bool">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>true<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.pager/contents/config/main.xml
+
+# Change window title default settings
+sed -i '/<entry name="altTxt"/,/<\/entry>/ s/<default>[^<]*<\/default>/<default><\/default>/' /usr/share/plasma/plasmoids/org.kde.windowtitle/contents/config/main.xml
+sed -i '/<entry name="txt"/,/<\/entry>/ s/<default>[^<]*<\/default>/<default><\/default>/' /usr/share/plasma/plasmoids/org.kde.windowtitle/contents/config/main.xml
+sed -i '/<entry name="txtSameFound"/,/<\/entry>/ s/<default>[^<]*<\/default>/<default><\/default>/' /usr/share/plasma/plasmoids/org.kde.windowtitle/contents/config/main.xml
+sed -i '/<entry name="maxminAllowed"/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>false<\/default>/' /usr/share/plasma/plasmoids/org.kde.windowtitle/contents/config/main.xml
+sed -i '/<entry name="closeAllowed"/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>false<\/default>/' /usr/share/plasma/plasmoids/org.kde.windowtitle/contents/config/main.xml
+sed -i '/<entry name="scrollAllowed"/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>false<\/default>/' /usr/share/plasma/plasmoids/org.kde.windowtitle/contents/config/main.xml
+
+# Change icons only task manager default settings
+sed -i '/<entry name="showOnlyCurrentDesktop"/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>false<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
+sed -i '/<entry name="wheelEnabled"/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>false<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
