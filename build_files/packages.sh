1fd005be686d012e29553bc1cde29010c7d96345
Marco Zanzottera <marcozanzottera@e.email>
2024-09-07 16:21:10 +0200

    Add polonium tiling script

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -31,13 +31,6 @@ mv papirus-icon-theme/Papirus-Dark /usr/share/icons/Papirus-Dark
 rm -rf papirus-icon-theme
 
 # Install polonium KWin script for tiling
-cd $(mktemp -d)
-curl -L https://github.com/zeroxoneafour/polonium/releases/latest/download/polonium.kwinscript -o polonium-kwinscript.zip
-# Workaround with main.js filename, should'n be necessary in future releases
-mkdir polonium-kwinscript
-unzip polonium-kwinscript.zip -d polonium-kwinscript
-mv polonium-kwinscript/pkg/contents/code/main.mjs polonium-kwinscript/pkg/contents/code/main.js
-cd polonium-kwinscript
-zip -r ../polonium-kwinscript.zip .
-cd ..
-kpackagetool6 --type KWin/Script --global --install polonium-kwinscript.zip
+
+curl -Lo /etc/yum.repos.d/eclipseo-polonium-fedora-$(cat /etc/os-release | grep VERSION_ID | cut -d '=' -f 2).repo https://copr.fedorainfracloud.org/coprs/eclipseo/polonium/repo/fedora-$(cat /etc/os-release | grep VERSION_ID | cut -d '=' -f 2)/eclipseo-polonium-fedora-$(cat /etc/os-release | grep VERSION_ID | cut -d '=' -f 2).repo
+rpm-ostree install polonium
