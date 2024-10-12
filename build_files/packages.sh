c64ca011e97734e6fcaf432d41dd13edd25c83ad
Marco Zanzottera <marcozanzottera@e.email>
2024-10-12 17:12:26 +0200

    Add ARG to select fedora version to use

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -10,7 +10,7 @@ RELEASE="$(rpm -E %fedora)"
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/${FEDORA_MAJOR_VERSION}/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Install sunshine from pre built package with DMA-BUF sharing for VAAPI support (pull #2053)
 # binary artifact has expired, rebuilding for now and adding this package manually
