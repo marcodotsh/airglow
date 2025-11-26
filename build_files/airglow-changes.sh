cfe0568d343b9e175c90188ba44b8d9f21d2a82c
Marco Zanzottera <marcozanzottera@e.email>
2025-11-26 18:54:32 +0100

    Remove nvidia and surface not available in F43

--- build_files/airglow-changes.sh
+++ build_files/airglow-changes.sh
@@ -5,7 +5,7 @@ set -ouex pipefail
 ### Airglow config changes to KDE Plasma
 
 # Set taskbar icons
-sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/filemanager,preferred:\/\/browser,applications:dev.vencord.Vesktop.desktop,applications:org.gnome.Ptyxis.desktop,applications:org.kde.discover.desktop<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
+sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/filemanager,preferred:\/\/browser,applications:dev.vencord.Vesktop.desktop,applications:org.gnome.Ptyxis.desktop,applications:io.github.kolunmi.Bazaar.desktop<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
 # Set kickoff menu icons
 #sed -i '/<entry name="favorites" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,systemsettings.desktop,org.kde.dolphin.desktop,org.kde.kate.desktop,org.gnome.Ptyxis.desktop,org.kde.discover.desktop<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.kickoff/contents/config/main.xml
 # Set icon theme
