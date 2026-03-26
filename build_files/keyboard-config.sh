ef8a5b8f8c9ee1d3a5efa11a8fa989bca6e8f36f
Marco Zanzottera <marcozanzottera@e.email>
2026-03-26 10:43:56 +0100

    Enable scroll_lock for all layouts

--- build_files/keyboard-config.sh
+++ build_files/keyboard-config.sh
@@ -7,5 +7,5 @@ RELEASE="$(rpm -E %fedora)"
 
 ### Keyboard Configuration
 
-# Configure it keyboard layout to support scroll lock for backlight keyboard
-sed -i '0,/.*include "level3(ralt_switch).*/s/.*include "level3(ralt_switch).*/    \/\/ Enable led on Kit Devastator Keyboard\n    modifier_map Mod3 { Scroll_Lock };\n\n&/' /usr/share/X11/xkb/symbols/it
+# Configure keyboard layout to support scroll lock for backlight keyboard
+sed -i 's/key <SCLK> {\[  Scroll_Lock  \]};/&\n    \/\/ Enable led on Kit Devastator Keyboard\n    modifier_map Mod3 { Scroll_Lock };/' /usr/share/X11/xkb/symbols/pc
