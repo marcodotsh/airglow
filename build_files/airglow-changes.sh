9ca2161548c6913ef00bf888ef47608dc7eb8cf8
Marco Zanzottera <marcozanzottera@e.email>
2026-03-26 10:16:34 +0100

    Change plasma settings customization to respect new approach

--- build_files/airglow-changes.sh
+++ build_files/airglow-changes.sh
@@ -4,20 +4,53 @@ set -ouex pipefail
 
 ### Airglow config changes to KDE Plasma
 
+LAYOUT_JS="/usr/share/plasma/look-and-feel/dev.getaurora.aurora.desktop/contents/layouts/org.kde.plasma.desktop-layout.js"
+
+# Change icons only task manager defaults
+sed -i '/widget.writeConfig("launchers"/,/widget.reloadConfig()/ {
+    /widget.reloadConfig()/i\
+            widget.writeConfig("showOnlyCurrentDesktop", "false");\
+            widget.writeConfig("wheelEnabled", "false");
+}' "$LAYOUT_JS"
+
 # Set taskbar icons
-sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/filemanager,preferred:\/\/browser,applications:dev.vencord.Vesktop.desktop,applications:org.gnome.Ptyxis.desktop,applications:io.github.kolunmi.Bazaar.desktop<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
-# Set kickoff menu icons
-#sed -i '/<entry name="favorites" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,systemsettings.desktop,org.kde.dolphin.desktop,org.kde.kate.desktop,org.gnome.Ptyxis.desktop,org.kde.discover.desktop<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.kickoff/contents/config/main.xml
+sed -i '/widget.writeConfig("launchers", \[/,/\]);/{
+    /widget.writeConfig("launchers", \[/,/\]);/c\
+            widget.writeConfig("launchers", [\
+                "preferred://filemanager",\
+                "preferred://browser",\
+                "applications:dev.vencord.Vesktop.desktop",\
+                "applications:org.gnome.Ptyxis.desktop",\
+                "applications:io.github.kolunmi.Bazaar.desktop"\
+            ]);
+}' "$LAYOUT_JS"
+
+# Configure pager defaults
+cat >> "$LAYOUT_JS" << 'PAGER_CONFIG'
+
+for (let i = 0; i < allPanels.length; ++i) {
+    const panel = allPanels[i];
+    const widgets = panel.widgets();
+
+    for (let j = 0; j < widgets.length; ++j) {
+        const widget = widgets[j];
+
+        if (widget.type === "org.kde.plasma.pager") {
+            widget.currentConfigGroup = ["General"];
+            widget.writeConfig("displayedText", "0");
+            widget.writeConfig("showWindowIcons", "true");
+            widget.writeConfig("showOnlyCurrentScreen", "true");
+            widget.writeConfig("wrapPage", "true");
+            widget.reloadConfig();
+        }
+    }
+}
+PAGER_CONFIG
+
 # Set icon theme
 sed -i '/\[Icons\]/,// s/^Theme=.*/Theme=Papirus-Dark/' /usr/share/kde-settings/kde-profile/default/share/config/kdeglobals
 sed -i '/\Icon Theme\]/,// s/^Inherits=.*/Inherits=breeze_cursors,Papirus-Dark/' /usr/share/icons/default/index.theme
 sed -i '/\[Icons\]/,// s/^Theme=.*/Theme=Papirus-Dark/' /usr/share/plasma/look-and-feel/dev.getaurora.aurora.desktop/contents/defaults
-
-# Change pager default settings
-sed -i '/<entry name="displayedText" type="Enum">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>0<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.pager/contents/config/main.xml
-sed -i '/<entry name="showWindowIcons" type="Bool">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>true<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.pager/contents/config/main.xml
-sed -i '/<entry name="showOnlyCurrentScreen" type="Bool">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>true<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.pager/contents/config/main.xml
-sed -i '/<entry name="wrapPage" type="Bool">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>true<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.pager/contents/config/main.xml
 
 # Change window title default settings
 sed -i '/<entry name="altTxt"/,/<\/entry>/ s/<default>[^<]*<\/default>/<default><\/default>/' /usr/share/plasma/plasmoids/org.kde.windowtitle/contents/config/main.xml
@@ -26,10 +59,6 @@ sed -i '/<entry name="txtSameFound"/,/<\/entry>/ s/<default>[^<]*<\/default>/<de
 sed -i '/<entry name="maxminAllowed"/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>false<\/default>/' /usr/share/plasma/plasmoids/org.kde.windowtitle/contents/config/main.xml
 sed -i '/<entry name="closeAllowed"/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>false<\/default>/' /usr/share/plasma/plasmoids/org.kde.windowtitle/contents/config/main.xml
 sed -i '/<entry name="scrollAllowed"/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>false<\/default>/' /usr/share/plasma/plasmoids/org.kde.windowtitle/contents/config/main.xml
-
-# Change icons only task manager default settings
-sed -i '/<entry name="showOnlyCurrentDesktop"/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>false<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
-sed -i '/<entry name="wheelEnabled"/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>false<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
 
 # Add to ujust airglow entries
 echo 'import "/usr/share/ublue-os/just/80-airglow.just"' >> /usr/share/ublue-os/justfile
